a63b1c46753a2c31ab7e112753200095ce30e247
carsonsx <carsonsx@qq.com>
2017-05-08 19:01:55 +0800

    update load command to support more cases

--- cmd/load.go
+++ cmd/load.go
@@ -2,29 +2,41 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/carsonsx/tcptester/cmd/input"
+	"github.com/carsonsx/tcptester/conf"
 	"github.com/carsonsx/tcptester/net"
 	"strconv"
 	"sync"
 	"time"
-	"github.com/carsonsx/tcptester/conf"
-	"github.com/carsonsx/tcptester/cmd/input"
 )
 
 type LoadCommand struct {
 	connectedClientCount int
-	mutex sync.Mutex
-	stop bool
+	mutex                sync.Mutex
+	stop                 bool
+}
+
+type LoadData struct {
+	BeginData  []*TestData
+	StressData []*TestData
+	EndData    []*TestData
+}
+
+type TestData struct {
+	GlobalData []byte
+	ClientData [][]byte
+	SleepTime int
 }
 
 type loadStat struct {
-	id int
-	sent int
-	success int
-	fail int
+	id       int
+	sent     int
+	success  int
+	fail     int
 	received int
 }
 
-func (s *loadStat) print()  {
+func (s *loadStat) print() {
 	if s.id == 0 {
 		fmt.Printf("total send %d, success %d, fail %d, received %d\n", s.sent, s.success, s.fail, s.received)
 	} else {
@@ -32,12 +44,20 @@ func (s *loadStat) print()  {
 	}
 }
 
-
-func (s *loadStat) add(another loadStat)  {
+func (s *loadStat) add(another *loadStat) {
 	s.sent += another.sent
 	s.success += another.success
 	s.fail += another.fail
 	s.received += another.received
+}
+
+func (s *loadStat) count(err error) {
+	s.sent++
+	if err == nil {
+		s.success++
+	} else {
+		s.fail++
+	}
 }
 
 func (c *LoadCommand) Name() string {
@@ -66,32 +86,40 @@ func (c *LoadCommand) Validate(tcpClient *net.TCPClient, args ...string) bool {
 
 func (c *LoadCommand) Run(tcpClient *net.TCPClient, args ...string) error {
 
-	testData := args[0]
+	inputTestData := args[0]
 	clientCount, _ := strconv.Atoi(args[1])
 	sendCount, _ := strconv.Atoi(args[2])
 	sendInterval := 0
 	if len(args) >= 4 {
 		sendInterval, _ = strconv.Atoi(args[3])
 	}
-	data, err := input.GetInstance(conf.Config.InputMode).Convert(testData)
+	data, err := input.GetInstance(conf.Config.InputMode).Convert(inputTestData)
 	if err != nil {
 		fmt.Println(err)
 		return err
 	}
+	testData := new(TestData)
+	testData.GlobalData = data
+	testData.SleepTime = sendInterval
+	loadData := new(LoadData)
+	loadData.StressData = []*TestData{testData}
+	return c.Load(tcpClient, loadData, clientCount, sendCount)
+}
 
-	//for _, d := range data {
+func (c *LoadCommand) Load(tcpClient *net.TCPClient, loadData *LoadData, clientCount int, sendCount int) error {
+	//for _, d := range loadData {
 	//	fmt.Printf("%d ", d)
 	//}
 	//fmt.Println()
 
-	done := make(chan loadStat)
+	done := make(chan *loadStat)
 
 	//ignore syncTime
 	syncTime := conf.Config.SyncTime
 	conf.Config.SyncTime = 0
 
 	for i := 1; i <= clientCount; i++ {
-		go c.start(tcpClient, i, data, sendCount, sendInterval, done)
+		go c.load(tcpClient, i, loadData, sendCount, done)
 	}
 
 	doneClientCount := 0
@@ -112,8 +140,9 @@ func (c *LoadCommand) Run(tcpClient *net.TCPClient, args ...string) error {
 	return nil
 }
 
+func (c *LoadCommand) load(tcpClient *net.TCPClient, clientId int, loadData *LoadData, sendCount int, done chan *loadStat) {
 
-func (c *LoadCommand) start(tcpClient *net.TCPClient, clientId int, data []byte, sendCount int, sendInterval int, done chan loadStat) {
+	// create new tcp client
 	newTcpClient := new(net.TCPClient)
 	defer newTcpClient.Close()
 	newTcpClient.Addr = tcpClient.Addr
@@ -121,9 +150,8 @@ func (c *LoadCommand) start(tcpClient *net.TCPClient, clientId int, data []byte,
 	newTcpClient.SetReader(tcpClient.GetReader())
 	err := newTcpClient.Connect()
 
-	var stat loadStat
+	stat := new(loadStat)
 	stat.id = clientId
-	stat.sent = sendCount
 
 	if err != nil {
 		fmt.Printf("client[%d] failed to connect %s: %v\n", clientId, newTcpClient.Addr, err)
@@ -132,22 +160,16 @@ func (c *LoadCommand) start(tcpClient *net.TCPClient, clientId int, data []byte,
 		c.connectedClientCount++
 		c.mutex.Unlock()
 		fmt.Printf("client[%d] established\n", clientId)
+
+		c.SendTestDataGroup(newTcpClient, clientId, loadData.BeginData, stat)
+
 		for j := 1; !c.stop && j <= sendCount; j++ {
-			_, err = newTcpClient.Write(data)
-			if err != nil {
-				fmt.Printf("client[%d] write data error: %v", clientId, err)
-				stat.fail++
-			} else {
-				//fmt.Printf("client[%d] write data done\n", clientId)
-				stat.success++
-				if sendInterval > 0 {
-					//fmt.Printf("client[%d] sleep %d milliseconds\n", clientId, sendInterval)
-					time.Sleep(time.Millisecond * time.Duration(sendInterval))
-				}
-			}
+			c.SendTestDataGroup(newTcpClient, clientId, loadData.StressData, stat)
 		}
 
-		//sleep 5 seconds for data receiving
+		c.SendTestDataGroup(newTcpClient, clientId, loadData.EndData, stat)
+
+		//sleep 5 seconds for loadData receiving
 		time.Sleep(5 * time.Second)
 		stat.received = newTcpClient.ReceivedCount
 
@@ -155,8 +177,37 @@ func (c *LoadCommand) start(tcpClient *net.TCPClient, clientId int, data []byte,
 
 	}
 
-
 	done <- stat
+}
+
+
+func (c *LoadCommand) SendTestDataGroup(tcpClient *net.TCPClient, clientId int, testDataGroup []*TestData, stat *loadStat)  {
+
+	if testDataGroup == nil || len(testDataGroup) == 0 {
+		return
+	}
+
+	var err error
+	for _, testData := range testDataGroup {
+		err = c.SendTestData(tcpClient, clientId, testData)
+		stat.count(err)
+	}
+}
+
+func (c *LoadCommand) SendTestData(tcpClient *net.TCPClient, clientId int, testData *TestData) error {
+	var err error
+	if testData.ClientData != nil {
+		_, err = tcpClient.Write(testData.ClientData[clientId-1])
+	} else {
+		_, err = tcpClient.Write(testData.GlobalData)
+	}
+	if err != nil {
+		fmt.Printf("client[%d] send data error: %v", clientId, err)
+	}
+	if testData.SleepTime > 0 {
+		time.Sleep(time.Millisecond * time.Duration(testData.SleepTime))
+	}
+	return err
 }
 
 func init() {
